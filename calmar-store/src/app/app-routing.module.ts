57541dd7f21101240e1d2baa3ab64538a0cae5e3
juan <jmcaldino@gmail.com>
2021-02-14 19:54:13 -0300

    added lazy loading in home

--- calmar-store/src/app/app-routing.module.ts
+++ calmar-store/src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 import { ProductsComponent } from './products/products.component';
 import { ContactComponent } from './contact/contact.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
@@ -18,7 +17,7 @@ const routes: Routes = [
       },
       {
         path: 'home',
-        component: HomeComponent
+        loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
       },
       {
         path: 'products',
