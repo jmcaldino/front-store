f21c2aa2ece12aff4977904dafb433b5340ae28c
juan <jmcaldino@gmail.com>
2021-02-16 20:18:08 -0300

    added put & delete method

--- calmar-store/src/app/core/services/products/products.service.ts
+++ calmar-store/src/app/core/services/products/products.service.ts
@@ -15,10 +15,23 @@ export class ProductsService {
   ) { }
 
   getAllProducts(): Observable<Product[]> {
-    return this.http.get<Product[]>(`${environment.url_api}/products/`);
+    return this.http.get<Product[]>(`${environment.url_api}/products`);
   }
 
   getProduct(id: string): Observable<Product>{
     return this.http.get<Product>(`${environment.url_api}/products/${id}`);
   }
+
+  createProduct(product: Product): Observable<Product>{
+    return this.http.post<Product>(`${environment.url_api}/products`, product);
+  }
+
+  updateProduct(id: string, changes: Partial<Product>): Observable<Product>{
+    return this.http.put<Product>(`${environment.url_api}/products/${id}`, changes);
+  }
+
+  deleteProduct(id: string): any{
+    return this.http.delete(`${environment.url_api}/products/${id}`);
+  }
+
 }
--- calmar-store/src/app/product/components/product-detail/product-detail.component.ts
+++ calmar-store/src/app/product/components/product-detail/product-detail.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Params } from '@angular/router';
 
 import { ProductsService } from './../../../core/services/products/products.service';
 import { Product } from './../../../core/models/product.model';
+import { subscribeOn } from 'rxjs/operators';
 
 @Component({
   selector: 'app-product-detail',
@@ -30,6 +31,38 @@ export class ProductDetailComponent implements OnInit {
     .subscribe(product => {
       this.product = product;
     })
+  }
+
+  createProduct(): void{
+    const newProduct: Product = {
+      id: '222',
+      title: 'nuevo desde angular',
+      image: 'assets/images/banner-1.jpg',
+      price: 3000,
+      description: 'nuevo product'
+    };
+    this.productsService.createProduct(newProduct)
+    .subscribe(product => {
+      console.log(product)
+    });
+  }
+
+  updateProduct(): void{
+    const updateProduct: Partial<Product> = {
+      price: 80000,
+      description: 'edicion product'
+    };
+    this.productsService.updateProduct('2',updateProduct)
+    .subscribe(product => {
+      console.log(product)
+    });
+  }
+
+  deleteProduct(): void{
+    this.productsService.deleteProduct('321')
+    .subscribe(product => {
+      console.log(product)
+    });
   }
 
 }
