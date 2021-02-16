a534042567ec525e4b098162eae9f818922555a7
juan <jmcaldino@gmail.com>
2021-02-15 22:35:18 -0300

    added custom footer

--- calmar-store/src/app/product/components/products/products.component.ts
+++ calmar-store/src/app/product/components/products/products.component.ts
@@ -12,29 +12,29 @@ export class ProductsComponent implements OnInit {
   products: Product[] = [
     {
       id: '1',
-      image: 'assets/images/camiseta.png',
-      title: 'Camiseta',
+      image: 'assets/images/1597109494558_escobillon-recto-n5-romyl-art-61-gaj.jpg',
+      title: 'Escobillon',
       price: 80000,
       description: 'bla bla bla bla bla'
     },
     {
       id: '2',
-      image: 'assets/images/hoodie.png',
-      title: 'Hoodie',
+      image: 'assets/images/1597109788293_CANASTO CALADO 50 LT.jpg',
+      title: 'Canasto',
       price: 80000,
       description: 'bla bla bla bla bla'
     },
     {
       id: '3',
-      image: 'assets/images/mug.png',
-      title: 'Mug',
+      image: 'assets/images/1597110057269_CABO HIERRO ROYCO.jpg',
+      title: 'Cabo Hierro',
       price: 80000,
       description: 'bla bla bla bla bla'
     },
     {
       id: '4',
-      image: 'assets/images/pin.png',
-      title: 'Pin',
+      image: 'assets/images/1597110228856_cabo de madera.jpg',
+      title: 'Cabo Madera',
       price: 80000,
       description: 'bla bla bla bla bla'
     },
