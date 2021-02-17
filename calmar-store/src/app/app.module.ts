fb375b997a78f3cee30c8bc41f5ea47defed3097
juan <jmcaldino@gmail.com>
2021-02-17 10:30:45 -0300

    added new stag environment.

--- calmar-store/src/app/app.module.ts
+++ calmar-store/src/app/app.module.ts
@@ -29,7 +29,7 @@ import { HttpClientModule } from '@angular/common/http';
     MatTableModule,
     MatPaginatorModule,
     MatSortModule,
-    HttpClientModule
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
