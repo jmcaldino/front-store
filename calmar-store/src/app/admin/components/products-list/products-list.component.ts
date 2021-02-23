348bcee01fc25c311bc14e8e221c90581456fb00
juan <jmcaldino@gmail.com>
2021-02-23 00:35:48 -0300

    filter view

--- calmar-store/src/app/admin/components/products-list/products-list.component.ts
+++ calmar-store/src/app/admin/components/products-list/products-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { ProductsService } from '../../../core/services/products/products.service';
+import { AfterViewInit, Component, ViewChild, OnInit } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource , MatTable} from '@angular/material/table';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -11,22 +12,41 @@ import { Router } from '@angular/router';
   templateUrl: './products-list.component.html',
   styleUrls: ['./products-list.component.scss']
 })
-export class ProductsListComponent implements OnInit {
+export class ProductsListComponent implements AfterViewInit {
 
-  products = [];
+  products: Product[] = [];
   displayedColumns: string[] = ['title', 'price', 'description', 'actions'];
+  dataSource: MatTableDataSource<Product>;
 
   private itemsCollection: AngularFirestoreCollection<Product>;
   items: Observable<Product[]>;
 
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.itemsCollection = this.afs.collection<Product>('products');
+    this.items = this.itemsCollection.valueChanges();
+    this.items.subscribe(product => {
+      if (product){
+        this.dataSource = new MatTableDataSource(product);
+        // Assign the paginator *after* dataSource is set
+        this.dataSource.paginator = this.paginator;
+      }else {
+        console.error('Unable to delete the product');
+      }
+    });
+    this.dataSource.paginator;
+  }
+
   constructor(
-    private productsService: ProductsService,
     private afs: AngularFirestore,
     private router: Router
   ) { }
 
-  ngOnInit(): void {
-    this.fetchProducts();
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim(); // Remove whitespace
+    this.dataSource.filter = filterValue.toLowerCase(); // MatTableDataSource defaults to lowercase matches
+    // this.dataSource.filter = filterValue;
   }
 
   fetchProducts(){
@@ -34,15 +54,6 @@ export class ProductsListComponent implements OnInit {
     // .subscribe(products => {
     //   this.products = products;
     // });
-    this.itemsCollection = this.afs.collection<Product>('products');
-    this.items = this.itemsCollection.valueChanges();
-    this.items.subscribe(product => {
-      if (product){
-        this.products = product
-      }else {
-        console.error('Unable to delete the product');
-      }
-    });
   }
 
   deleteProduct(id: string): void{
